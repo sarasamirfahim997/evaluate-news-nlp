b5b7d8431be838a9101fbda407515870e09abb0c
Sara <sarasamirfahim997@gmail.com>
2021-12-01 19:32:57 +0200

    service worker change

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -31,6 +32,7 @@ module.exports = {
             verbose: true,
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
